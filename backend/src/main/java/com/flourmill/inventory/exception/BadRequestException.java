55e5d75b92ccd00167546124aa48491018076b73
Abhishek Jaiswal <98872450+Abhijais2003@users.noreply.github.com>
2026-03-28 15:03:01 +0530

    Delete backend directory

--- backend/src/main/java/com/flourmill/inventory/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.flourmill.inventory.exception;
-
-public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
--- backend/src/main/java/com/flourmill/inventory/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.flourmill.inventory.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
